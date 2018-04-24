02b41491b8f3a377c2d359148a691c6656b540f8
mohamed ahmed <mohamedahmedelsaid99@gmail.com>
2018-04-24 10:47:19 -0700

    round 2 is complete we wating for your task bro!

--- chatroom/consumers.py
+++ chatroom/consumers.py
@@ -1,13 +1,28 @@
 from channels.generic.websocket import WebsocketConsumer
-
+from index.models import Room, Masseages
 import json
 
 class loadprevious (WebsocketConsumer):
 
-    def connect(self):
+    def connect(self): # check the user acsess room he not joined (forget)
+
         self.room_name = self.scope['url_route']['kwargs']['roomName']
-        print(self.room_name)
+        room = Room.objects.filter(name=self.room_name).first()
+        pervious_masseages = room.masseages.order_by('-pk')[:7] #limiting the masseages to be seven
+        # check that the room has no masseages
+        if pervious_masseages.first() is None:
+            self.accept()
+            return
+
+        listofchat = list()
+        for item in pervious_masseages:
+            pieceofchat = {"user": item.sender.username, "text": item.text, "pk": item.pk}
+            listofchat.append(pieceofchat)
+
         self.accept()
+        self.send(text_data=json.dumps({"masseages": listofchat}))
+
+
 
 
     def close(self, code=None):
@@ -15,4 +30,14 @@ class loadprevious (WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
 
-        pass
+        data = json.loads(text_data)
+        id = data['id']
+        room = Room.objects.filter(name=self.room_name).first()
+        pervious_masseages = room.masseages.filter(pk__lt=id).order_by('-pk')[:7]
+
+        listofchat = list()
+        for item in pervious_masseages:
+            pieceofchat = {"user": item.sender.username, "text": item.text, "pk": item.pk}
+            listofchat.append(pieceofchat)
+
+        self.send(text_data=json.dumps({"masseages": listofchat}))
